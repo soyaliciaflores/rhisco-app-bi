b1a62c8a1bc6f3a2c589134bcfd681e10e906b09
AndreVivs <75138276+AndreVivs@users.noreply.github.com>
2021-05-27 13:22:47 -0500

    added axios and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "react-app",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+  },
+  plugins: ["react"],
+  rules: {},
 };
