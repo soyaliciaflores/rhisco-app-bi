001de637de95da5c938b7bb34ba25a77e2d4c187
ravalidesign <ravalidesign@gmail.com>
2021-05-29 12:51:03 -0500

    Tabla y Grafica de Historical Prices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
+import HistoricalPrice from './Graficos/Historical_Price/HistoricalPrice'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rhisco</h1>
+        <HistoricalPrice/>
       </header>
     </div>
   );
