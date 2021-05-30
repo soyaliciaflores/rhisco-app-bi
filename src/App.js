4e80f9134cb87ed0628ae0d59b439b017479076b
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-05-29 22:18:18 -0500

    Renderizado de la Grafica de AssetsLocation, instalacioón de la dependencia chart-js 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
+import AssetAllocation from './Graficos/Asset_Allocation/AssetAllocation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rhisco</h1>
+          <AssetAllocation />
       </header>
     </div>
   );
