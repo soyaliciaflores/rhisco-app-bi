e80aaa9c31710dbb82aec3d81e160a6fde0b6f1a
Mariliz G <marilizgonzalez1@gmail.com>
2021-05-30 18:39:42 -0500

    Instanación de dependencias faltantes, modificación de JSON y tabla Holdings

--- src/Graficos/Holdings/Holdings.jsx
+++ src/Graficos/Holdings/Holdings.jsx
@@ -1,8 +1,130 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
+//import Holdings from '../../ArchivosJSON/Holdings.json'
+import axios from 'axios'
+import DataTable from 'react-data-table-component'
 
-const Holdings = () => {
+
+const columns= [
+    
+        {
+            name:'Fund',
+            selector: 'Fund', 
+            sortable: true,
+            center: true,
+            wrap: true
+        },
+        {
+            name:'Company',
+            selector: 'Company', 
+            sortable: true,
+            center: true,
+            wrap: true
+           
+        },
+        {
+            name:'ISIN',
+            selector: 'ISIN', 
+            sortable: true,
+            center: true
+        },
+        {
+            name:'1 year change',
+            selector: '1 year change', 
+            sortable: true,
+            center: true,
+            
+        },
+        {
+            name:'Portfolio weight',
+            selector: 'Portfolio weight', 
+            sortable: true,
+            center: true
+        },
+        // {
+        //     name:'Long allocation',
+        //     selector: 'Long allocation', 
+        //     sortable: true
+        // },
+        
+    ] 
+
+    
+    const dataAPI ='https://rhisco-89b36-default-rtdb-holdings.firebaseio.com/.json'
+    
+    const Holdings = () => {
+    
+    
+        const [data, setData] = useState([]);
+        console.log(data)
+        const apiRequest = async () => {
+            await axios.get(dataAPI)
+                .then(res => {
+                    const data = res.data
+                    
+                    setData(data)
+                    
+    
+                })
+        }
+        useEffect(() => {
+           apiRequest();
+        }, [])
+    
+        // createTheme('holdingsGreen', {
+        //     text: {
+        //       primary: '#000000',
+        //       secondary: '#FFFFF',
+        //     },
+        //     background: {
+        //       default: '#9CBABA',
+        //     },
+        //     context: {
+        //       background: '#00000',
+        //       text: '#FFFFFF',
+        //     },
+        //     divider: {
+        //       default: '#0000000',
+        //     },
+            
+            
+        //   });
+
+          const customStyles = {
+            
+            rows: {
+              style: {
+                minHeight: '80px', // override the row height
+              }
+            },
+            headCells: {
+              style: {
+                paddingLeft: '8px', // override the cell padding for head cells
+                paddingRight: '8px',
+                
+              },
+            },
+            cells: {
+              style: {
+                paddingLeft: '8px', // override the cell padding for data cells
+                paddingRight: '8px',
+              },
+            },
+          };
+
     return ( 
         <Fragment>
+           <div className="tablePrueba">
+               <DataTable
+               columns={columns}
+               data={data}
+               title= 'Holdings'
+               center
+               theme="holdingsGreen"
+               customStyles={customStyles}
+               allowOverflow
+               
+               />
+           </div>
         </Fragment>
      );
 }
