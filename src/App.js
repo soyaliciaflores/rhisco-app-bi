fc86fc1245201dbede53ae974df9279277c9a78c
AndreVivs <75138276+AndreVivs@users.noreply.github.com>
2021-05-30 20:32:07 -0500

    add rout fund list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import AssetAllocation from './Graficos/Asset_Allocation/AssetAllocation';
 import HistoricalPrice from './Graficos/Historical_Price/HistoricalPrice';
 import Holdings from './Graficos/Holdings/Holdings';
 import Return from './Graficos/Return/Return';
+import FilterReturn from './Graficos/Return/FilterReturn';
+import FundList from './Graficos/Fund_List/FundList';
 import Sectors from './Graficos/Sector/Sector';
 import NavGraph from './General/Navigation/NavGraph';
 import NavTable from './General/Navigation/NavTable';
@@ -12,11 +14,13 @@ import './Graficos/Return/FilterReturn.css'
 import './General/Navigation/NavGraph.css'
 import './General/Navigation/NavTable.css'
 import './Graficos/Sector/Sector.css';
+import './Graficos/Fund_List/FundList.css';
 
 function App() {
   
   return (
     <div className="App">
+        <FundList/>
         <FilterReturn/>
         <NavGraph/>
         <NavTable/>
--- src/Graficos/Fund_List/FundList.jsx
+++ src/Graficos/Fund_List/FundList.jsx
@@ -1,10 +1,83 @@
-import React, {Fragment} from 'react';
-
+import React, {Fragment, useEffect, useState} from 'react';
+import '../../App.css';
+import axios from 'axios'
+import DataTable from 'react-data-table-component';
+// import dbreturn from '../../ArchivosJSON/Return.json'
+const columnas = [
+    {
+        name:"Fund Type",
+        selector:"Fund Type",
+        sortable:true
+    },
+    {
+        name:"Name",
+        selector:"Name",
+        sortable:true
+    },
+    {
+        name:"Group Name",
+        selector:"Group Name",
+        sortable:true
+    },
+    {
+        name:"Fund Launch",
+        selector:"Fund Launch",
+        sortable:true
+    },
+    {
+        name:"Domicile",
+        selector:"Domicile",
+        sortable:true
+    },
+    {
+        name:"ISIN",
+        selector:"ISIN",
+        sortable:true
+    },
+    {
+        name:"Price Currency",
+        selector:"Price currency",
+        sortable:true
+    },
+    {
+        name:"Sector",
+        selector:"Sector",
+        sortable:true
+    },
+    {
+        name:"Asset Class",
+        selector:"Asset Class",
+        sortable:true
+    }
+]
+const url = 'https://rhisco-89b36-default-rtdb-40b73.firebaseio.com/.json';
 const FundList = () => {
+    const [data, setData] = useState([]);
+    const peticionApi = async () =>{
+        await axios.get(url)
+        .then(resp =>{
+          setData(resp.data);
+          // setFund();
+          console.log(resp)
+        })
+        .catch(err =>{
+            console.log('Error: ', err)
+        })
+    }
+    useEffect(() => {
+        peticionApi();
+    }, [])
     return ( 
         <Fragment>
+            { data && data.map( fund => (
+                <DataTable
+                title = "Fund List"
+                columns = {columnas}
+                data = {fund}
+                key = {fund.ISIN}
+                />
+            ))}
         </Fragment>
      );
 }
- 
 export default FundList;
