db2d0a8bf165cc34ca43104d9c541c16240b05da
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-06-02 08:29:08 -0500

    arreglando detallitos de css

--- src/General/Navigation/Navigation.jsx
+++ src/General/Navigation/Navigation.jsx
@@ -6,10 +6,10 @@ const Navigation = () => {
         <div className='main-container-navigation'>
             <img className='image-navigation'src={rhisco_blackout_logo} alt='logo de Rhisco'/>
             <div className='container-buttons-navigation'>
-                <Link to='/directivo'>
+                <Link to='/executive'>
             <button className='dir-analyst-button-navig'> Executive </button>
                 </Link>
-            <Link to='/analista'>
+            <Link to='/analyst'>
                 <button className='dir-analyst-button-navig'> Analyst </button>
             </Link>
             </div>
